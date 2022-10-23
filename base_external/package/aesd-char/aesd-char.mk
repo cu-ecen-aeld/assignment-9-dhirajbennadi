f4f8b68f972f778f3c09883a71fb000c788396dc
Dhiraj Bennadi <dhiraj.bennadi@colorado.edu>
2022-10-23 00:57:14 -0600

    AESD Char Socket Implementation

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -8,7 +8,7 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #Dhiraj Bennadi
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_CHAR_VERSION = 'f91dddd2a3f89a1de9be10903318e5a383b4de7e'
+AESD_CHAR_VERSION = '1aa159ae92368caef7b139dbb7d63f628ff80229'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
